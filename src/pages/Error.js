610b5ef27a5f9e7d85737c61835088052da5af60
Lisa Capelle <capelle.box@gmail.com>
2023-02-21 00:05:50 +0100

    Error page style

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "../styles/Error.css";
 
 export default function Error() {
   return (
     <section className="Error">
       <p className="Error__code">404</p>
-      
+      <div className="Error__msg">
+        <span>Oups! La page que </span>
+        <span>vous demandez n'existe pas.</span>
+      </div>
+      <Link to="/">Retourner sur la page d’accueil</Link>
     </section>
   );
 }
