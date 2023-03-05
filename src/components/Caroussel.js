c50e56a1e6dbac6ab526dd319e683aacce576c78
Lisa Capelle <capelle.box@gmail.com>
2023-03-05 23:37:33 +0100

    Caroussel

--- src/components/Caroussel.js
+++ src/components/Caroussel.js
@@ -6,16 +6,36 @@ export default function Caroussel({pictures}) {
   const [currentPage, setCurrentPage] = useState(1)
 
   
+  const previousSlide = () => {
+ 
+    if(currentPage === 1 ) {
+      setCurrentPage(pictures.length)
+    }else{
+      setCurrentPage(currentPage - 1)
+    }
+    
+  }
+
+  const nextSlide = () => {
+    if(currentPage > pictures.length - 1) {
+      setCurrentPage(1)
+    }else{
+      setCurrentPage(currentPage+1)
+    }
+    
+  }
+
   return (
     <section className="Caroussel">
       <div className="Caroussel__img-wrap">
-        IMAGE
+        {<img  className="Caroussel__img" src={pictures[currentPage-1]} alt="caroussel" />}
       </div>
-   
+      
+      <i className="fa-solid fa-chevron-left Carousel__chevron-left" onClick = {previousSlide}></i>
+      <i className="fa-solid fa-chevron-right Carousel__chevron-right" onClick = {nextSlide}></i>
    
       <div className="Caroussel__pages">
-        
-        
+        <span className="Caroussel__current">{currentPage}</span>/<span className="Caroussel__total">{pictures.length}</span>
       </div>
     </section>
   );
