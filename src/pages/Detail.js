f8906c3609a4dcd42edd3b8b00ac9af71c51f487
Lisa Capelle <capelle.box@gmail.com>
2023-03-06 12:03:29 +0100

    redirection to error page if !id

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,4 +1,5 @@
-// import React, { useEffect, useState } from "react";
+import React from 'react';
+import {useEffect} from 'react';
 import Caroussel from "../components/Caroussel";
 import Collapse from "../components/Collapse";
 import CollapseContent from "../components/CollapseContent";
@@ -15,46 +16,60 @@ export default function Detail({apartmentList}) {
 
   const navigate = useNavigate();
 
-   
-      const apartment = apartmentList.find(element => element.id === id)
-    //   console.log(apartment);
+    useEffect(() => {
+      const apartment = apartmentList.find(element => element.id === id);
       if(!apartment) {
-        navigate('*')
-      }else{
-        return (
-            <main className="Detail">
-              <Caroussel pictures = {apartment.pictures} />
-              <section className="Detail__info">
-                <div className="Detail__location-wrap">
-                  <div className="Detail__location">
-                    <h2 className="Detail__location__title">
-                      {apartment.title}
-                    </h2>
-                    <p className="Detail__location__city">{apartment.location}</p>
-                    <Hashtags tags = {apartment.tags} />
-                  </div>
-                  <div className="Detail__evaluation">
-                    <div className="Detail__evaluation__host">
-                      <p className="host__name">{apartment.host.name}</p>
-                      <img
-                        className="host__avatar"
-                        src={apartment.host.picture}
-                        alt=""
-                      />
-                    </div>
-                    <Stars rating = {apartment.rating}/>
-                  </div>
-                </div>
-                <div className="Detail__content-wrap">
-                  <Collapse title="Description">
-                    <CollapseContent content={apartment.description} />
-                  </Collapse>
-                  <Collapse title="Équipements">
-                    <CollapseContent equipments = {apartment.equipments}/>
-                  </Collapse>
-                </div>
-              </section>
-            </main>
-          );
+        navigate('*');
       }
+      // else{
+
+     
+
+      // }
+    })
+      
+    //   console.log(apartment);
+    const apartment = apartmentList.find(element => element.id === id);
+    console.log(apartment);
+    if (apartment !== undefined) {
+      return (
+        <main className="Detail">
+          <Caroussel pictures = {apartment.pictures} />
+          <section className="Detail__info">
+            <div className="Detail__location-wrap">
+              <div className="Detail__location">
+                <h2 className="Detail__location__title">
+                  {apartment.title}
+                </h2>
+                <p className="Detail__location__city">{apartment.location}</p>
+                <Hashtags tags = {apartment.tags} />
+              </div>
+              <div className="Detail__evaluation">
+                <div className="Detail__evaluation__host">
+                  <p className="host__name">{apartment.host.name}</p>
+                  <img
+                    className="host__avatar"
+                    src={apartment.host.picture}
+                    alt=""
+                  />
+                </div>
+                <Stars rating = {apartment.rating}/>
+              </div>
+            </div>
+            <div className="Detail__content-wrap">
+              <Collapse title="Description">
+                <CollapseContent content={apartment.description} />
+              </Collapse>
+              <Collapse title="Équipements">
+                <CollapseContent equipments = {apartment.equipments}/>
+              </Collapse>
+            </div>
+          </section>
+        </main>
+      );  
+    }
+
+    
+
+      
 }
