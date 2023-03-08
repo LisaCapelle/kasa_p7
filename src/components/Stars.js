3b454f5838109c97ac6eb42bf2ccb2103c983354
Lisa Capelle <capelle.box@gmail.com>
2023-03-08 22:58:29 +0100

    Stars

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,5 +1,24 @@
-export default function Stars (){
-    return <div>
-        Stars
-    </div>
+import React from "react";
+
+import "../styles/Stars.css";
+
+export default function Stars({ rating }) {
+//   console.log(rating);
+  const lis = [1, 2, 3, 4, 5];
+
+  return (
+    <ul className="Rating">
+      { lis.map((el, index) => 
+        el <= rating ? (
+          <li  key={index}>
+            <i className="fa-solid fa-star "></i>
+          </li>
+        ) : (
+          <li key={index} className="Stars-grey">
+            <i className="fa-solid fa-star"></i>
+          </li>
+        )
+      )}
+    </ul>
+  )
 }
