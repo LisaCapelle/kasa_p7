c192553467c6e513d961d08d4caf79849084c96d
Lisa Capelle <capelle.box@gmail.com>
2023-03-08 23:50:59 +0100

    CollapseContent

--- src/components/CollapseContent.js
+++ src/components/CollapseContent.js
@@ -2,8 +2,17 @@ import React from "react";
 
 import "../styles/CollapseContent.css";
 
-export default function CollapseContent (){
-    return <div>
-        COLLAPSE content
-    </div>
+export default function CollapseContent({content, equipments}) {
+
+  if (equipments) {
+    return (
+      <ul className="CollapseContent">
+        {equipments.map((element, index) => (
+          <li key={index} > {element}</li>
+        ))}
+      </ul>
+    );
+  } else {
+    return <p className="CollapseContent">{content}</p>;
+  }
 }
