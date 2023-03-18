ee7bc141d651ae3dd50b1548a4dd8b71177d7cc6
Lisa Capelle <capelle.box@gmail.com>
2023-03-18 15:23:30 +0100

    console.log removed in Hashtags

--- src/components/Hashtags.js
+++ src/components/Hashtags.js
@@ -3,8 +3,6 @@ import React from 'react';
 import '../styles/Hashtags.css';
 
 export default function Hashtags({ tags }) {
-      console.log(tags);
-
       return (
             <ul className="Hashtags">
                   {tags.map((city, index) => (
