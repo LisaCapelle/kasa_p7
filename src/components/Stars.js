462a4ba375ad0f4a42a62517e9dc77e447557183
Lisa Capelle <capelle.box@gmail.com>
2023-03-18 17:15:39 +0100

    improvements to css and code layout

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -3,11 +3,11 @@ import React from 'react';
 import '../styles/Stars.css';
 
 export default function Stars({ rating }) {
-      const lis = [1, 2, 3, 4, 5];
+      const list = [1, 2, 3, 4, 5];
 
       return (
             <ul className="Rating">
-                  {lis.map((el, index) =>
+                  {list.map((el, index) =>
                         el <= rating ? (
                               <li key={index}>
                                     <i className="fa-solid fa-star "></i>
